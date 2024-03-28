9b85dd12a14ea124fe54a86776af8d1aeeedc33a
Brian Ngetich <kipronobrian97@gmail.com>
2024-03-28 07:50:44 +0300

    Added scope functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName (){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    const changeLeastFavoriteCustomer = "";
+    return changeLeastFavoriteCustomer = "bob";
+}
